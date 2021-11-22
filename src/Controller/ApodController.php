1cd835080ee48cace91345ccca3a3a0f70a12837
Elodie Bichet <e_bichet@yahoo.fr>
2021-11-22 17:49:23 +0100

    Add controller and template to display the most recent image

---
+++ src/Controller/ApodController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\MediaRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ApodController extends AbstractController
+{
+    protected $mediaRepository;
+
+    public function __construct(MediaRepository $mediaRepository)
+    {
+        $this->mediaRepository = $mediaRepository;
+    }
+
+    /**
+     * @Route("/apod", name="apod")
+     */
+    public function show(): Response
+    {
+        $picture = $this->mediaRepository->findOneBy(['media_type' => 'image'], ['date' => 'DESC']);
+
+        return $this->render('apod/index.html.twig', [
+            'picture' => $picture
+        ]);
+    }
+}
