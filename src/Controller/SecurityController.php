c42fa94271056d1f7cd93f33f8baba02116df638
Elodie Bichet <e_bichet@yahoo.fr>
2021-11-23 12:28:06 +0100

    Update security config and form, add bundles and create user fixtures

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,19 +2,36 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\LoginType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     /**
      * @Route("/login", name="security_login")
      */
-    public function login(): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
+        $form = $this->createForm(LoginType::class);
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
         return $this->render('security/login.html.twig', [
-            'controller_name' => 'SecurityController',
+            'error' => $error,
+            'form' => $form->createView()
         ]);
+    }
+
+    /**
+     * @Route("/logout", name="security_logout")
+     */
+    public function logout()
+    {
+        // throw new \LogicException('This method can be blank
+        // - it will be intercepted by the logout key on your firewall.');
     }
 }
---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+    protected $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        // Create fake users
+        for ($u = 0; $u < 9; $u++) {
+            $user = new User();
+            $hash = $this->encoder->encodePassword($user, "password");
+            $user->setEmail("user$u@email.com")
+                ->setPassword($hash);
+
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+}
