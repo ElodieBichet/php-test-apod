0cb9b3f4b44588e72f37e100b067d4ace5a65886
Elodie Bichet <e_bichet@yahoo.fr>
2021-11-23 14:45:01 +0100

    Create Media fixtures

---
+++ src/DataFixtures/MediaFixtures.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Media;
+use App\Service\CallApodAPIService;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class MediaFixtures extends Fixture
+{
+    protected $callApodAPIService;
+
+    public function __construct(CallApodAPIService $callApodAPIService)
+    {
+        $this->callApodAPIService = $callApodAPIService;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        // Get APOD of the 5 last days
+        for ($d = 5; $d >= 0; $d--) {
+            $date = date('Y-m-d', strtotime("- $d day"));
+            /** @var Media */
+            $media = $this->callApodAPIService->createMediaFromAPOD($date);
+            $manager->persist($media);
+        }
+
+        $manager->flush();
+    }
+}
